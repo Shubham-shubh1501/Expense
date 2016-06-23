8a923aa70c2922c2a2d897d44855cec1091907e6
Shubham Jain S <shubham.shubh1501@gmail.com>
2016-06-23 23:02:10 +0530

    Profilecontroller.groovy

--- grails-app/controllers/expense/ProfileController.groovy
+++ grails-app/controllers/expense/ProfileController.groovy
@@ -1,11 +1,34 @@
 package expense
-
 class ProfileController {
 
     def index() {
-
+        if(!session.loggedInUserID) {
+            redirect(controller: "Login" , action: "index")
+        }
+        Users details=Users.findById(session.loggedInUserID);
+        [sendDetails: details]
     }
     def edit() {
-
+        if(!session.loggedInUserID) {
+            redirect(controller: "Login" , action: "index")
+        }
+        Users details=Users.findById(session.loggedInUserID);
+        [sendDetails: details]
+    }
+    def save() {
+        Users myUser=new Users([firstName:params.firstName,lastName:params.lastName ,email:params.Email,
+                                password:params.password])
+        if(hasErrors()){
+            render(action: "edit",[myUser1: myUser])
+        }
+        println(params)
+        Users register = Users.get(params.id)
+        register.firstName = params.firstName;
+        register.lastName = params.lastName;
+        register.email = params.Email;
+        println(register.firstName+" "+register.lastName+" "+register.email)
+        register.save(flush: true);
+        println("updated")
+        redirect(action: "index")
     }
 }
