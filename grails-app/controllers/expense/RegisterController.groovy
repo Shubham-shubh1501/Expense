2993f6c3d969b54374a1e0665c5636ed97ca2843
Shubham Jain S <shubham.shubh1501@gmail.com>
2016-06-23 23:02:58 +0530

    RegisterController.groovy

--- grails-app/controllers/expense/RegisterController.groovy
+++ grails-app/controllers/expense/RegisterController.groovy
@@ -9,10 +9,12 @@ class RegisterController {
     def save() {
         Map p = [firstName: params.firstName, lastName: params.lastName, email: params.Email, password: params.password]
         Users register= new Users(p)
-        register.save()
+
         if (register.hasErrors()) {
             render(view: "index", model: [myUser: register])
             return
         }
+        register.save()
+        [registeredUser: register]
     }
 }
