a798fdfd5ec3df384be54388bbe209bc37a75e7c
Shubham Jain S <shubham.shubh1501@gmail.com>
2016-06-23 23:00:12 +0530

    logincontoller.groovy

--- grails-app/controllers/expense/LoginController.groovy
+++ grails-app/controllers/expense/LoginController.groovy
@@ -1,29 +1,24 @@
 package expense
 
 class LoginController {
-
     def index() {
-    [myUser:new Users()]
+        [myUser:new Users()]
     }
     def save() {
-    println(params);
-    Map p=[email:params.Email ,password: params.password]
-    def c = Users.createCriteria()
-    def results = c {
-    eq("email", "${params.Email}")
-    eq("password", "${params.password}")
-    }
-
-    if(results) {
-        session.recent = session.recent ?: []
-        session.recent.add(results)
-        println(session.recent)
-        redirect(controller: "Profile" , action:"index",send: session.recent)
-    }
-    else{
-        redirect(action: "index")
-    }
+        println(params);
+        Users results = Users.createCriteria().get {
+            eq("email", "${params.Email}")
+            eq("password", "${params.password}")
+        }
 
 
+        if(results) {
+            session.loggedInUserID = results.id
+            println(session.loggedInUserID)
+            redirect(controller: "Profile" , action:"index")
+        }
+        else{
+            redirect(action: "index")
+        }
     }
 }
